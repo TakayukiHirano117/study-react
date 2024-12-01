69cee8688a50771740e5836693f3897e6d7bed15
TakayukiHirnao117 <ikusa.hirano@icloud.com>
2024-12-01 21:14:39 +0900

    ⚡️ useCallback, useEffectの学習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import styles from "@/styles/Home.module.css";
 import { Footer } from "@/components/Footer";
 import Main from "@/components/Main";
 import Header from "@/components/Header";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,12 +18,16 @@ const geistMono = localFont({
 });
 
 export default function Home() {
-  const [foo, setFoo] = useState(1);
+  const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    setFoo((prevFoo) => prevFoo + 1);
-  }
+  const handleClick = useCallback((e) => {
+    console.log(count)
+    if(count < 10) {
+      setCount((prevCount) => prevCount + 1);  
+    }
+
+    // setCount((prevCount) => prevCount + 1);
+  }, [count])
   
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -45,7 +49,7 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <Header />
-        <h1>{foo}</h1>
+        <h1>{count}</h1>
         <button href="/about" onClick={handleClick}>
           ボタン
         </button>
